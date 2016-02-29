b4e2d9624a078b91c76b29af2b280c8d692d2b12
Kim Nielsen <kgn@one.com>
2016-02-29 10:12:36 +0100

    Bumped version and added spec

---
+++ spec/role_local_spec.rb
@@ -0,0 +1,88 @@
+# vim: syntax=ruby:expandtab:shiftwidth=2:softtabstop=2:tabstop=2
+
+# Copyright 2013-2014 Facebook
+# Copyright 2014-present One.com
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'spec_helper'
+require 'chef_diff/changes/change'
+require 'chef_diff/changes/role_local'
+require 'logger'
+
+describe ChefDiff::Changes::RoleLocal do
+  let(:logger) do
+    Logger.new('/dev/null')
+  end
+  let(:role_local_dir) do
+    'roles_local'
+  end
+
+  fixtures = [
+    {
+      :name => 'empty filelists',
+      :files => [],
+      :result => [],
+    },
+    {
+      :name => 'delete role',
+      :files => [
+        {
+          :status => :deleted,
+          :path => 'roles_local/test.json'
+        },
+        {
+          :status => :modified,
+          :path => 'cookbooks/two/cb_one/metadata.rb'
+        },
+      ],
+      :result => [
+        ['test', :deleted],
+      ],
+    },
+    {
+      :name => 'add/modify a environment specific role',
+      :files => [
+        {
+          :status => :modified,
+          :path => 'cookbooks/one/cb_one/recipes/test.rb'
+        },
+        {
+          :status => :modified,
+          :path => 'roles_local/cluster/test.json'
+        },
+        {
+          :status => :modified,
+          :path => 'cookbooks/one/cb_one/recipes/test3.rb'
+        },
+      ],
+      :result => [
+        ['cluster/test', :modified],
+      ],
+    },
+  ]
+
+  fixtures.each do |fixture|
+    it "should handle #{fixture[:name]}" do
+      ChefDiff::Changes::RoleLocal.find(
+        fixture[:files],
+        role_local_dir,
+        logger
+      ).map do |cb|
+        [cb.full_name, cb.status]
+      end.
+      should eq(fixture[:result])
+    end
+  end
+
+end
